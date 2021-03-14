133380981c04c11fc782366657062c3a2834340f
Murky MoMo <70970380+Murky-MoMo@users.noreply.github.com>
2021-03-14 07:01:24 -0400

    Fixed mute.js
    `const Authorized = Message.member.roles.cache.find(role => role.id === Config.Config.Leader_Role)`
     Instead of using .find() and searching for ids, .get() should be used, Discord.Collections are extended maps with some array methods added, we can capitalize on that using <Map>.get(<key>) instead of <Array>.find(<Callback>), that would be faster and overall more efficient. The Collection for roles is mapped by the role IDs so the key would be the id
    
    `if(UserID.length != 18 || !isNumeric(UserID)) {`
    the Discord IDs are snowflakes, the length depends on time, meaning, in the future the length can be 19 not just that, existing members who created their accounts a long time ago have an id length of 17 
    
    the entire UserID part was removed and replaced because this way of getting an id is flawed and doesn't guarantee that an actual member will be grabbed, i can use <@!012345678912345678>, that's a random id that would get the pass, in the case of <@!ID>, it's better to use message.mentions.members.first() as that already returns a GuildMember Instance and should that fail ...cache.get("ID") would be searching for the member using the id
    
    as mentioned above, the code doesn't deal with MuteTarget not being defined which will lead to an error
    `if(!MuteTarget) return Message.reply(`Please mention a valid role or provide a valid role id`)` was added for that
    
    `const MuteRole = await Message.guild.roles.fetch(Config.Config.Muted_Role);`
    fetch returns a promise, resolves if the role exists, it rejects and throws an error in case the role doesn't exist, not a falsy value, so the if case previously used below won't work as expected, however the .get() returns undefined should the role not exist
    
    await the roles.add() so that it sends the message right after the role gets added
    
    speaking of promises, most of the promises can throw an error, for now i added a catch to it, there was almost no error handling in the code
    Added a few if cases to handle errors a bit more

--- SeraphBot/commands/mute.js
+++ SeraphBot/commands/mute.js
@@ -10,36 +10,25 @@ const Run = async function(Message){
     if(!Config.Config.Leader_Role) return Message.reply(`You must set the \`Leader_Role\` option to a server role the Current value is \`${Config.Leader_Role}\``);
     
     //Checks if user is allowed to use the command
-    const Authorized = Message.member.roles.cache.find(role => role.id === Config.Config.Leader_Role);
-    if(!Authorized) return;
+    if(!Message.guild.me.hasPermission("MANAGE_ROLES") return Message.channel.send(`I don't have permissions to give roles`).catch(console.error)  //checks if the bot has permissions to add roles
 
+    const Authorized = Message.member.roles.cache.get(Config.Config.Leader_Role);
+    if(!Authorized) return;
     const args = Message.content.split(' ');
 
-    
-    let UserID = null;
-    if(args[1].startsWith('<@!')){
-        UserID = args[1].replace('<@!','').replace('>','')
-    }else UserID = args[1]
-    if(UserID.length != 18 || !isNumeric(UserID)) {
-        return Message.reply(`Invalid user given for the Mute command`);
-    }
+    await Message.guild.members.fetch().catch(console.error)  //fetch to fill up the <Guild>.members
+    const MuteTarget = Message.mentions.members.first() || Message.guild.members.cache.get(args[1])
+    //* Enable privalaged intents in the Developer portal or this won't work 
+    if(!MuteTarget) return Message.reply(`Please mention a valid role or provide a valid role id`).catch(console.error)
+    const MuteRole = Message.guild.role.cache.get(Config.Config.Muted_role)
+    if(!MuteRole) return Message.reply(`The role with the id \`Config.Config.Muted_Role\` is invalid or doesn't exist in this server`).catch(console.error);
+    if(!MuteRole.editable) return Message.channel.send(`I don't have permissions to give out the muted role`).catch(console.error);  // Checks if the bot can send out the role
 
-    const MuteTarget = await Message.guild.members.fetch(UserID)
-    const MuteRole = await Message.guild.roles.fetch(Config.Config.Muted_Role);
-    if(!MuteRole) return Message.reply(`The role with the id \`Config.Config.Muted_Role\` is invalid or doesn't exist in this server`);
-
-
-    MuteTarget.roles.add(MuteRole);
-    Message.reply(`${MuteTarget.user.tag} has been muted`)
+    await MuteTarget.roles.add(MuteRole).catch(console.error);
+    Message.reply(`${MuteTarget.user.tag} has been muted`).catch(console.error)
 }
 
-module.exports ={
+module.exports = {
     Run,
     Help
 }
-
-function isNumeric(str) {
-    if (typeof str != "string") return false // we only process strings!  
-    return !isNaN(str) && // use type coercion to parse the _entirety_ of the string (`parseFloat` alone does not do this)...
-           !isNaN(parseFloat(str)) // ...and ensure strings of whitespace fail
-  }
